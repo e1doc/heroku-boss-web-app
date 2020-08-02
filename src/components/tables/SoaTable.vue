efcaaf1d659b27b448b3c882441b487350ff0f2e
System Administrator <joshua@smartwave.ph>
2020-08-02 21:53:34 +0800

    Tables behavior

--- src/components/tables/SoaTable.vue
+++ src/components/tables/SoaTable.vue
@@ -1,23 +1,47 @@
 <template>
   <section>
-    <div class="thead">
-      <div class="th w10">ACCOUNT #</div>
-      <div class="th w10">REFERENCE #</div>
-      <div class="th w10">DATE</div>
-      <div class="th w35">BUSINESS NAME</div>
-      <div class="th w25">AMOUNT</div>
-      <div class="th w10">ACTIONS</div>
+    <div v-if="currentType === 'business'">
+      <div class="thead">
+        <div class="th w10">ACCOUNT #</div>
+        <div class="th w10">REFERENCE #</div>
+        <div class="th w10">DATE</div>
+        <div class="th w35">BUSINESS NAME</div>
+        <div class="th w25">AMOUNT</div>
+        <div class="th w10">ACTIONS</div>
+      </div>
+      <div class="tbody">
+        <div class="tr" v-for="index in 5" :key="index">
+          <div class="td w10">F-02248</div>
+          <div class="td w10">102582</div>
+          <div class="td w10">JUN 1, 2020</div>
+          <div class="td w35">MAMICHELLE FOOD STATION</div>
+          <div class="td w25">₱ 28,063.00</div>
+          <div class="td w10 actions">
+            <div class="bill" @click="redirect()">
+              <font-awesome-icon icon="receipt" class="mr5 icon" />PAY NOW
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="tbody">
-      <div class="tr" v-for="index in 5" :key="index">
-        <div class="td w10">F-02248</div>
-        <div class="td w10">102582</div>
-        <div class="td w10">JUN 1, 2020</div>
-        <div class="td w35">MAMICHELLE FOOD STATION</div>
-        <div class="td w25">₱ 28,063.00</div>
-        <div class="td w10 actions">
-          <div class="bill" @click="redirect()">
-            <font-awesome-icon icon="receipt" class="mr5 icon" />PAY NOW
+    <div v-if="currentType === 'real_property'">
+      <div class="thead">
+        <div class="th w15">TD #</div>
+        <div class="th w15">REFERENCE #</div>
+        <div class="th w15">DATE</div>
+        <div class="th w25">AMOUNT</div>
+        <div class="th w15">ACTIONS</div>
+      </div>
+      <div class="tbody">
+        <div class="tr" v-for="index in 5" :key="index">
+          <div class="td w15">F-02248</div>
+          <div class="td w15">102582</div>
+          <div class="td w15">JUN 1, 2020</div>
+          <div class="td w25">₱ 28,063.00</div>
+          <div class="td w15 actions">
+            <div class="bill" @click="redirect()">
+              <font-awesome-icon icon="receipt" class="mr5 icon" />PAY NOW
+            </div>
           </div>
         </div>
       </div>
@@ -26,14 +50,17 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "SoaTable",
-  methods:{
-    redirect(){
-      this.$router.push({path:'payment'})
-    }
-  }
-
+  methods: {
+    redirect() {
+      this.$router.push({ path: "payment" });
+    },
+  },
+  computed: {
+    ...mapGetters(["currentType"]),
+  },
 };
 </script>
 
@@ -41,11 +68,20 @@ export default {
 .w10 {
   width: 10%;
 }
-.w35 {
-  width: 35%;
+.w15 {
+  width: 25%;
 }
 .w25 {
   width: 25%;
+}
+.w30 {
+  width: 30%;
+}
+.w35 {
+  width: 35%;
+}
+.w45 {
+  width: 45%;
 }
 .thead {
   display: flex;
@@ -76,22 +112,23 @@ export default {
       text-align: center;
       padding: 17px 0px;
     }
-    .actions{
-        padding: 7px 0px;
+    .actions {
+      padding: 7px 0px;
     }
   }
 }
 
 .bill {
+  max-width: 117px;
+  margin: 0 auto;
   font-size: 13px;
-  padding: 8px 13px;
+  padding: 8px 0px;
   border: 2px #039be5 solid;
   border-radius: 5px;
   transition: 0.4s;
   cursor: pointer;
-  color: #039BE5;
+  color: #039be5;
   font-weight: bold;
-  margin-right: 15px;
 }
 .bill:hover {
   color: #ffffff !important;
