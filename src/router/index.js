3afcdb4cfeb906f4b28605b3a0d283ba23e18b52
System Administrator <joshua@smartwave.ph>
2020-08-10 00:07:53 +0800

    Application Static screens and auth implementation

--- src/router/index.js
+++ src/router/index.js
@@ -19,16 +19,16 @@ const routes = [
     path: "/",
     name: "Login",
     component: Login,
-  //  async beforeEnter(to, from, next) {
-  //     let hasPermission = await store.state.service.isAuthenticated;
-  //     if(hasPermission){
-  //       next({
-  //         name: "Profile"
-  //       })
-  //     }else{
-  //       next()
-  //     }
-  // }
+   async beforeEnter(to, from, next) {
+      let hasPermission = await store.state.service.isAuthenticated;
+      if(hasPermission){
+        next({
+          name: "Profile"
+        })
+      }else{
+        next()
+      }
+  }
   },
   {
     path: "/reset-password/:uid/:token",
@@ -81,16 +81,16 @@ const routes = [
         component: BuildingPermitApplication
       },
     ],
-  //  async beforeEnter(to, from, next) {
-  //       let hasPermission = await store.state.service.isAuthenticated;
-  //       if(hasPermission){
-  //         next()
-  //       }else{
-  //         next({
-  //           name: "Login"
-  //         })
-  //       }
-  //   }
+   async beforeEnter(to, from, next) {
+        let hasPermission = await store.state.service.isAuthenticated;
+        if(hasPermission){
+          next()
+        }else{
+          next({
+            name: "Login"
+          })
+        }
+    }
   },
   // {
   //   path: '/about',
