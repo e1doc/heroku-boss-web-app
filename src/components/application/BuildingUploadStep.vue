3db650acbb515f612da208defcd3e5e18a498b71
System Administrator <joshua@smartwave.ph>
2020-08-22 23:43:13 +0800

    Fix draft

--- src/components/application/BuildingUploadStep.vue
+++ src/components/application/BuildingUploadStep.vue
@@ -43,35 +43,35 @@
         name="tct"
         fileLabel="tct"
         :properties="getProperty('tct')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2. Tax Declaration of Lot"
         name="taxdeclaration"
         fileLabel="taxt_declaration"
         :properties="getProperty('taxt_declaration')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="3. Tax Clearance of Real Property for Lot (updated)"
         name="taxclearance"
         fileLabel="tax_clearance"
         :properties="getProperty('tax_clearance')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="4. Barangay Clearance (for construction - original copy)"
         name="brgyclearance"
         fileLabel="barangay_clearance"
         :properties="getProperty('barangay_clearance')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="5. Homeowner's Clearance (for construction - original copy)"
         name="homeownerclearance"
         fileLabel="home_owner_clearance"
         :properties="getProperty('home_owner_clearance')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="6. Affidavit of Undertaking"
@@ -79,7 +79,7 @@
         name="undertakingaffidavit"
         fileLabel="undertaking_affidavit"
         :properties="getProperty('undertaking_affidavit')"
-        type='property'
+        type="property"
       />
     </div>
     <div class="meta-form-group mb60">
@@ -89,28 +89,28 @@
         name="unifiedapplicationform"
         fileLabel="unified_application_form"
         :properties="getProperty('unified_application_form')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2a. Ancillary Forms - Architectural Permit"
         name="ancillaryarchitectural"
         fileLabel="architectural_permit"
         :properties="getProperty('architectural_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2b. Ancillary Forms - Civil/Structural Permit"
         name="ancillarycivil"
         fileLabel="civil_structural_permit"
         :properties="getProperty('civil_structural_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2c. Ancillary Forms - Sanitary/Plumbing Permit"
         name="ancillarysanitary"
         fileLabel="sanitary_plumbing_permit"
         :properties="getProperty('sanitary_plumbing_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2d. Ancillary Forms - Electrical Permit"
@@ -126,42 +126,42 @@
         name="ancillaryelectrical"
         fileLabel="electrical_permit"
         :properties="getProperty('electrical_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2e. Ancillary Forms - Mechanical Permit"
         name="ancillarymechanical"
         fileLabel="mechanical_permit"
         :properties="getProperty('mechanical_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2f. Ancillary Forms - Sign Permit"
         name="ancillarysign"
         fileLabel="sign_permit"
         :properties="getProperty('sign_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2g. Ancillary Forms - Electronic Permit"
         name="ancillaryelectronic"
         fileLabel="electronic_permit"
         :properties="getProperty('electronic_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="3. Locational Clearance Application Form"
         name="locationalclearance"
         fileLabel="location_clearance"
         :properties="getProperty('location_clearance')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="4. Contractor's Temporary Business Permit Application Form"
         name="contractorbusinesspermit"
         fileLabel="contractor_business_permit"
         :properties="getProperty('contractor_business_permit')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="5. Relocation Survey Report and Lot Plan with Vicinity Map"
@@ -169,7 +169,7 @@
         name="relocationsurvey"
         fileLabel="relocation_survey"
         :properties="getProperty('relocation_survey')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="6. Photocopy of updated valid PRC ID's and PTR of Engrs/Arch"
@@ -177,7 +177,7 @@
         name="prcid"
         fileLabel="prc_id"
         :properties="getProperty('prc_id')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="7. Project Specification"
@@ -185,7 +185,7 @@
         name="projspecification"
         fileLabel="project_specification"
         :properties="getProperty('project_specification')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="8. Notarized Bill of Materials"
@@ -193,7 +193,7 @@
         name="billofmaterials"
         fileLabel="bill_of_materials"
         :properties="getProperty('bill_of_materials')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="9. Structural Design Analysis and Computation"
@@ -201,7 +201,7 @@
         name="structuraldesign"
         fileLabel="structural_design"
         :properties="getProperty('structural_design')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="10. Geotech Report / Soil Boring Test Report"
@@ -209,7 +209,7 @@
         name="geotechreport"
         fileLabel="geotech_report"
         :properties="getProperty('geotech_report')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="11. Selsmic Analysis"
@@ -217,7 +217,7 @@
         name="selsmicanalysis"
         fileLabel="selsmic_analysis"
         :properties="getProperty('selsmic_analysis')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="12. Complete and Detailed Building Plans"
@@ -230,7 +230,7 @@
         name="buildingplans"
         fileLabel="building_plans"
         :properties="getProperty('building_plans')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="13. Construction Safety and Health Program (DOLE)"
@@ -238,7 +238,7 @@
         name="constructionsafety"
         fileLabel="construction_safety"
         :properties="getProperty('construction_safety')"
-        type='property'
+        type="property"
       />
     </div>
     <div class="meta-form-group mb60">
@@ -249,14 +249,14 @@
         name="sketchpin"
         fileLabel="sketch_pin"
         :properties="getProperty('sketch_pin')"
-        type='property'
+        type="property"
       />
       <base-file-uploader
         label="2. Picture of site/location (colored copy)"
         name="pictureofsite"
         fileLabel="picture_of_site"
         :properties="getProperty('picture_of_site')"
-        type='property'
+        type="property"
       />
     </div>
     <div class="meta-form-group button-left-right">
@@ -283,8 +283,8 @@ export default {
     ButtonBlock,
     BaseFileUploader,
   },
-  mounted(){
-    this.getRequirements()
+  mounted() {
+    this.getRequirements();
   },
   computed: {
     ...mapGetters(["buildingApplicationRequirements", "buildingRequirements"]),
@@ -294,8 +294,8 @@ export default {
       this.$store.commit("setCurrentApplicationStep", "2");
     },
     async nextStep() {
-      let payload = {is_draft: false}
-      await this.$store.dispatch('updateBuildingApplication',payload)
+      let payload = { is_draft: false };
+      await this.$store.dispatch("updateBuildingApplication", payload);
       this.$store.commit("setCurrentApplicationStep", "4");
     },
     async getRequirements() {
@@ -307,14 +307,27 @@ export default {
       if (this.buildingRequirements.id) {
         if (this.buildingRequirements.buildingrequirements.length > 0) {
           let property = {};
-          property = this.buildingRequirements.buildingrequirements.find((element) => {
-            if (element.requirements_label === key) {
-              return element;
+          property = this.buildingRequirements.buildingrequirements.find(
+            (element) => {
+              if (element.requirements_label === key) {
+                return element;
+              }
             }
-          });
+          );
           return property;
         }
       }
+    },
+  },
+  watch: {
+    draftProperty: {
+      deep: true,
+      handler(status) {
+        if (status) {
+          this.$router.push({ name: "Profile" });
+          this.$store.commit("setDraftProperty", false);
+        }
+      },
     },
   },
 };
@@ -409,86 +422,82 @@ div.meta-container .meta-custom-upload:hover div.meta-text {
   border-color: #2699fb !important;
 }
 
-
-
-
 /*
 MOBILE RESPONSIVENESS 
 --------------------------------------------------------------*/
 
-@media only screen and ( max-width : 1380px ){
-    div.meta-container h1.meta-form-title{
-        font-size: 22px;
-    }
+@media only screen and (max-width: 1380px) {
+  div.meta-container h1.meta-form-title {
+    font-size: 22px;
+  }
 }
 
-@media only screen and ( max-width : 768px ){
-    div.meta-container h1.meta-form-title{
-        font-size: 20px;
-    }
+@media only screen and (max-width: 768px) {
+  div.meta-container h1.meta-form-title {
+    font-size: 20px;
+  }
 
-    .meta-upload-box .meta-label{
-        font-size: 15px;
-    }
+  .meta-upload-box .meta-label {
+    font-size: 15px;
+  }
 
-    div.meta-container .meta-note{
-        font-size: 15px;
-        text-align: center;
-    }
+  div.meta-container .meta-note {
+    font-size: 15px;
+    text-align: center;
+  }
 
-    div.meta-container div.meta-form-group div.meta-group-title{
-        font-size: 16px;
-        margin-bottom: 20px;
-    }
+  div.meta-container div.meta-form-group div.meta-group-title {
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
 }
 
-@media only screen and ( max-width : 650px ){
-    div.meta-container{
-        padding: 40px 30px;
-    }
+@media only screen and (max-width: 650px) {
+  div.meta-container {
+    padding: 40px 30px;
+  }
 
-    div.meta-container h1.meta-form-title{
-        font-size: 18px;
-        margin-bottom: 30px;
-    }
+  div.meta-container h1.meta-form-title {
+    font-size: 18px;
+    margin-bottom: 30px;
+  }
 }
 
+@media only screen and (max-width: 480px) {
+  div.meta-container {
+    padding: 30px 15px;
+  }
 
-@media only screen and ( max-width : 480px ){
-    div.meta-container{
-        padding: 30px 15px;
-    }
+  div.meta-container h1.meta-form-title {
+    font-size: 16px;
+  }
 
-    div.meta-container h1.meta-form-title{
-        font-size: 16px;
-    }
+  div.meta-container div.meta-form-group div.meta-group-title {
+    font-size: 15px;
+  }
 
-    div.meta-container div.meta-form-group div.meta-group-title{
-        font-size: 15px;
-    }
-    
-    div.meta-container div.button-left-right .back-button{
-        background-color: #048cff;
-        border-color: #73befc;
-        width: auto;
-        background: transparent;
-        border: none;
-        box-shadow: none;
-        color: #2699fb;
-        min-width: unset;
-    }
+  div.meta-container div.button-left-right .back-button {
+    background-color: #048cff;
+    border-color: #73befc;
+    width: auto;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    color: #2699fb;
+    min-width: unset;
+  }
 
-    div.meta-container div.button-left-right .next-button{
-        width: auto;
-        background: transparent;
-        border: none;
-        box-shadow: none;
-        min-width: unset;
-        color: #2699fb;
-    }
+  div.meta-container div.button-left-right .next-button {
+    width: auto;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    min-width: unset;
+    color: #2699fb;
+  }
 
-    div.meta-container .meta-note{
-        font-size: 14px;
-    }
+  div.meta-container .meta-note {
+    font-size: 14px;
+  }
 }
 </style>
--- src/components/application/BusinessUploadStep.vue
+++ src/components/application/BusinessUploadStep.vue
@@ -79,15 +79,15 @@ export default {
     ...mapGetters(["applicationRequirements", "requirements"]),
   },
   mounted() {
-      this.getRequirements()
+    this.getRequirements();
   },
   methods: {
     previousStep() {
       this.$store.commit("setCurrentApplicationStep", "2");
     },
     async nextStep() {
-      let payload = {is_draft: false}
-      await this.$store.dispatch('updateBusinessApplication',payload)
+      let payload = { is_draft: false };
+      await this.$store.dispatch("updateBusinessApplication", payload);
       this.$store.commit("setCurrentApplicationStep", "4");
     },
     async getRequirements() {
@@ -96,17 +96,28 @@ export default {
       }
     },
     getProperty(key) {
-        if(this.requirements.id){
-            if (this.requirements.requirements.length > 0){
-               let property = {}
-               property = this.requirements.requirements.find(element=>{
-                    if(element.requirements_label === key){
-                        return element
-                    }
-                })
-                return property
+      if (this.requirements.id) {
+        if (this.requirements.requirements.length > 0) {
+          let property = {};
+          property = this.requirements.requirements.find((element) => {
+            if (element.requirements_label === key) {
+              return element;
             }
+          });
+          return property;
         }
+      }
+    },
+  },
+  watch: {
+    draftBusiness: {
+      deep: true,
+      handler(status) {
+        if (status) {
+          this.$router.push({ name: "Profile" });
+          this.$store.commit("setDraftBusiness", false);
+        }
+      },
     },
   },
 };
@@ -156,10 +167,10 @@ div.meta-container {
       transition: 0.4s;
     }
     .meta-note {
-        font-size: 16px;
-        color: #7ac0fa;
-        margin-bottom: 30px;
-        width: 100%;
+      font-size: 16px;
+      color: #7ac0fa;
+      margin-bottom: 30px;
+      width: 100%;
     }
     .meta-text.small {
       color: #64b4f9;
@@ -195,77 +206,73 @@ div.meta-container .meta-custom-upload:hover div.meta-text {
   border-color: #2699fb !important;
 }
 
-
-
-
 /*
 MOBILE RESPONSIVENESS 
 --------------------------------------------------------------*/
 
-@media only screen and ( max-width : 1380px ){
-    div.meta-container h1.meta-form-title{
-        font-size: 22px;
-    }
+@media only screen and (max-width: 1380px) {
+  div.meta-container h1.meta-form-title {
+    font-size: 22px;
+  }
 }
 
-@media only screen and ( max-width : 768px ){
-    div.meta-container h1.meta-form-title{
-        font-size: 20px;
-    }
+@media only screen and (max-width: 768px) {
+  div.meta-container h1.meta-form-title {
+    font-size: 20px;
+  }
 
-    .meta-upload-box .meta-label{
-        font-size: 15px;
-    }
+  .meta-upload-box .meta-label {
+    font-size: 15px;
+  }
 
-    div.meta-container .meta-note{
-        font-size: 15px;
-        text-align: center;
-    }
+  div.meta-container .meta-note {
+    font-size: 15px;
+    text-align: center;
+  }
 }
 
-@media only screen and ( max-width : 650px ){
-    div.meta-container{
-        padding: 40px 30px;
-    }
+@media only screen and (max-width: 650px) {
+  div.meta-container {
+    padding: 40px 30px;
+  }
 
-    div.meta-container h1.meta-form-title{
-        font-size: 18px;
-        margin-bottom: 30px;
-    }
+  div.meta-container h1.meta-form-title {
+    font-size: 18px;
+    margin-bottom: 30px;
+  }
 }
 
+@media only screen and (max-width: 480px) {
+  div.meta-container {
+    padding: 30px 15px;
+  }
 
-@media only screen and ( max-width : 480px ){
-    div.meta-container{
-        padding: 30px 15px;
-    }
+  div.meta-container h1.meta-form-title {
+    font-size: 16px;
+  }
 
-    div.meta-container h1.meta-form-title{
-        font-size: 16px;
-    }
-    
-    div.meta-container div.button-left-right .back-button{
-        background-color: #048cff;
-        border-color: #73befc;
-        width: auto;
-        background: transparent;
-        border: none;
-        box-shadow: none;
-        color: #2699fb;
-        min-width: unset;
-    }
+  div.meta-container div.button-left-right .back-button {
+    background-color: #048cff;
+    border-color: #73befc;
+    width: auto;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    color: #2699fb;
+    min-width: unset;
+  }
 
-    div.meta-container div.button-left-right .next-button{
-        width: auto;
-        background: transparent;
-        border: none;
-        box-shadow: none;
-        min-width: unset;
-        color: #2699fb;
-    }
+  div.meta-container div.button-left-right .next-button {
+    width: auto;
+    background: transparent;
+    border: none;
+    box-shadow: none;
+    min-width: unset;
+    color: #2699fb;
+  }
 
-    div.meta-container .meta-note{
-        font-size: 14px;
-    }
+  div.meta-container .meta-note {
+    font-size: 14px;
+  }
 }
 </style>
