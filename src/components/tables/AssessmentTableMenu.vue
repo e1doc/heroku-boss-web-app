0d1bdef71a62af7c2735d629615bfd731913fc61
Mae <mae@carlocab.com>
2020-11-07 23:39:22 +0800

    Fix AssessmentTableMenu responsive issues

--- src/components/tables/AssessmentTableMenu.vue
+++ src/components/tables/AssessmentTableMenu.vue
@@ -1,28 +1,53 @@
 <template>
-  <div class="menu-holder">
-    <div class="left-div flex-center">
-      <div class="menu-type">
-        <div
-            :class="{ active: currentType === 'business' }"
-            @click="changeType('business')"
-            v-if="groups.includes('superadmin') || groups.includes('business_application_approver') || groups.includes('business_application_read')"
-        >
-          <font-awesome-icon icon="store" class="mr5 icon" />
-          BUSINESS
+  <div class="table-menu">
+    <div class="menu-holder">
+        <div class="left-div flex-center">
+          <div class="menu-type">
+            <div
+                :class="{ active: currentType === 'business' }"
+                @click="changeType('business')"
+                v-if="groups.includes('superadmin') || groups.includes('business_application_approver') || groups.includes('business_application_read')"
+            >
+              <font-awesome-icon icon="store" class="mr5 icon" />
+              BUSINESS
+            </div>
+          </div>
+          <div class="menu-type">
+            <div
+                :class="{ active: currentType === 'real_property' }"
+                @click="changeType('real_property')"
+                v-if="groups.includes('superadmin') || groups.includes('building_application_approver') || groups.includes('building_application_read')"
+            >
+              <font-awesome-icon icon="city" class="mr5 icon" />
+              BUILDING
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="menu-type">
-        <div
-            :class="{ active: currentType === 'real_property' }"
-            @click="changeType('real_property')"
-            v-if="groups.includes('superadmin') || groups.includes('building_application_approver') || groups.includes('building_application_read')"
-        >
-          <font-awesome-icon icon="city" class="mr5 icon" />
-          BUILDING
+
+        <!-- DESKTOP FOR ASSESSMENT/ASSESSED TABS -->
+        <div class="right-div flex-wrap">
+            <div class="menu-type blue hide-in-mobile">
+                <div
+                :class="{ active: activeTab === 'for_assessment' }"
+                @click="changeTab('for_assessment')">
+                    <font-awesome-icon icon="list-alt" class="mr5 icon" />
+                    FOR ASSESSMENT
+                </div>
+            </div>
+            <div class="menu-type green hide-in-mobile">
+                <div
+                :class="{ active: activeTab === 'assessed' }"
+                @click="changeTab('assessed')">
+                    <font-awesome-icon icon="check" class="mr5 icon" />
+                    ASSESSED
+                </div>
+            </div>
+          <base-input-search v-model="search"  @keyup.native="searchData()"/>
         </div>
-      </div>
     </div>
-    <div class="right-div flex-wrap">
+
+    <!-- MOBILE FOR ASSESSMENT/ASSESSED TABS -->
+    <div class="right-div menu-mobile">
         <div class="menu-type blue">
             <div
             :class="{ active: activeTab === 'for_assessment' }"
@@ -39,7 +64,6 @@
                 ASSESSED
             </div>
         </div>
-      <base-input-search v-model="search"  @keyup.native="searchData()"/>
     </div>
   </div>
 </template>
@@ -160,38 +184,81 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.menu-holder {
-  background: #ffffff;
+.table-menu {
   width: 100%;
   display: flex;
-  flex-direction: row;
-  padding: 10px 20px;
-  border-radius: 8px;
-  box-shadow: 0px 10px 20px #0000000d;
-  .left-div {
-    width: 50%;
-      .menu-type {
-        div{
-            color: #f09795;
-            font-weight: bold;    
-            font-size: 14px;
-            margin-left: 15px;
-            cursor: pointer;
-            padding: 15px 15px 18px;
-            margin-bottom: -11px;
-            transition: 0.3s;
-            border-bottom: 3px solid #fff;
+  flex-wrap: wrap;
+
+  .menu-holder{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    background: #ffffff;
+    padding: 10px 20px;
+    border-radius: 8px;
+    box-shadow: 0px 10px 20px #0000000d;
+    .left-div {
+      width: 50%;
+        .menu-type {
+          div{
+              color: #f09795;
+              font-weight: bold;    
+              font-size: 14px;
+              margin-left: 15px;
+              cursor: pointer;
+              padding: 15px 15px 18px;
+              margin-bottom: -11px;
+              transition: 0.3s;
+              border-bottom: 3px solid #fff;
+          }
+          .active {
+              border-color: #e23a36;
+              color: #e23a36;
+              font-weight: bold;
+          }
         }
-        .active {
-            border-color: #e23a36;
-            color: #e23a36;
-            font-weight: bold;
+    }
+    .right-div {
+      width: 50%;
+      justify-content: flex-end;
+      .menu-type {
+        font-weight: bold;
+        font-size: 13px;
+        margin-right: 15px;
+        cursor: pointer;
+        div {
+          padding: 10px;
+          border: 3px solid #fff;
+          opacity: 0.5;
+        }
+        div.active {
+          border-color: #fadddd;
+          border-radius: 5px;
+          opacity: 1;
         }
       }
+      .menu-type.blue{
+          color: #1492E6  ;
+          div.active {
+              border-color: #1492E6;
+          }
+      }
+      .menu-type.green{
+          color: #4caf50;
+          div.active {
+              border-color: #4caf50;
+          }
+      }
+    }
   }
-  .right-div {
-    width: 50%;
+
+  .right-div.menu-mobile{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
     justify-content: flex-end;
+    margin-top: 15px;
+    display: none;
     .menu-type {
       font-weight: bold;
       font-size: 13px;
@@ -220,6 +287,15 @@ export default {
             border-color: #4caf50;
         }
     }
+  }
+}
+
+@media only screen and( max-width: 1480px ){
+  body .table-menu .right-div.menu-mobile{
+      display: flex;
+  }
+  .right-div .hide-in-mobile{
+      display: none;
   }
 }
 </style>
