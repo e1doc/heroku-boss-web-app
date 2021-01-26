f96acadb929ca6b7e02509176c7418d8bbc19180
Mae <mae@carlocab.com>
2021-01-27 02:29:13 +0800

    Payment Modals

--- src/components/payment/PaymentDetailsModal.vue
+++ src/components/payment/PaymentDetailsModal.vue
@@ -1,63 +1,74 @@
 <template>
-  <section>
-    <div>
-      <div class="flex-column">
-        <h3 class="mb20">Please enter the payment details below:</h3>
-        <div class="meta-input-label mt10 mb10">Bank Used to Pay</div>
-        <base-input
-          v-model="payment_details.bank"
-          label="Enter bank name"
-          name="bank"
-          refs="payment_bank"
-          type="text"
-          class="mt40"
-          :validationMessages="stepOneErrors.payment_details.bank"
-        />
-        <div class="meta-input-label mt10 mb10">Date of Payment</div>
-        <base-date-picker
-          v-model="payment_details.date"
-          class="mb15"
-          :validationMessages="stepOneErrors.payment_details.date"
-        />
-        <div class="flex-wrap">
-          <div class="meta-input-label mt10 mb10">Total Amount Paid</div>
-          <currency-input
-            label="Total Amount Paid"
-            name="totalamount"
-            refs="total_amount"
-            type="text"
-            :isAmount="true"
-            class="mt40 w100"
-            placeholder="Enter total amount paid"
-            v-model="payment_details.amount"
-            :validationMessages="stepOneErrors.payment_details.amount"
-          />
+  <div class="modal flex-center">
+      <div class="meta-modal">
+        <div class="meta-wrap">
+          <div class="meta-modal-body flex-wrap">
+              <h3 class="mb20">Please enter the payment details below:</h3>
+              <div class="meta-input-group">
+                  <div class="meta-input-label mt10 mb10">Bank Used to Pay</div>
+                  <base-input
+                    v-model="payment_details.bank"
+                    label="Enter bank name"
+                    name="bank"
+                    refs="payment_bank"
+                    type="text"
+                    class="mt40 input-field"
+                    :validationMessages="stepOneErrors.payment_details.bank"
+                  />
+              </div>
+              <div class="meta-input-group">
+                  <div class="meta-input-label mt10 mb10">Date of Payment</div>
+                  <base-date-picker
+                    v-model="payment_details.date"
+                    class="mb15 input-field"
+                    :validationMessages="stepOneErrors.payment_details.date"
+                  />
+              </div>
+              <div class="flex-wrap meta-input-group">
+                  <div class="meta-input-label mt10 mb10">Total Amount Paid</div>
+                  <currency-input
+                    label="Total Amount Paid"
+                    name="totalamount"
+                    refs="total_amount"
+                    type="text"
+                    :isAmount="true"
+                    class="mt40 w100 input-field amount-paid"
+                    placeholder="Enter total amount paid"
+                    v-model="payment_details.amount"
+                    :validationMessages="stepOneErrors.payment_details.amount"
+                  />
+              </div>
+              <div class="meta-input-group">
+                  <div class="meta-input-label mt10 mb10">Reference No.</div>
+                  <base-input
+                    label="Enter reference no."
+                    name="refrenceno"
+                    refs="reference_no"
+                    type="text"
+                    class="mt40 input-field"
+                    v-model="payment_details.reference_no"
+                    :validationMessages="stepOneErrors.payment_details.reference_no"
+                  />
+              </div>
+              <div class="meta-upload-div flex-wrap">
+                <base-file-uploader
+                  label="Upload proof of transaction:"
+                  name="payment_details"
+                  fileLabel="paymentdetails"
+                  type="business"
+                  class="mt15 custom-upload"
+                  :isPaymentDetails="true"
+                  :hasError="uploadHasError"
+                />
+              </div>
+          </div>
+          <div class="meta-buttons flex-wrap">
+              <button class="modal-button agree" @click.native="submit">SUBMIT</button>
+              <button class="modal-button cancel" @click="onClickCallback(false)">CANCEL</button>
+          </div>
         </div>
-        <div class="meta-input-label mt10 mb10">Reference No.</div>
-        <base-input
-          label="Enter reference no."
-          name="refrenceno"
-          refs="reference_no"
-          type="text"
-          class="mt40"
-          v-model="payment_details.reference_no"
-          :validationMessages="stepOneErrors.payment_details.reference_no"
-        />
-        <div class="mt40">
-          <base-file-uploader
-            label="Upload screenshot of transaction:"
-            name="payment_details"
-            fileLabel="paymentdetails"
-            type="business"
-            class="mt30 custom-upload"
-            :isPaymentDetails="true"
-            :hasError="uploadHasError"
-          />
-        </div>
-        <button-block @click.native="submit">Submit</button-block>
       </div>
-    </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -158,14 +169,72 @@ export default {
 </script>
 
 <style scoped lang="scss">
-section {
-  padding: 25px 25px 35px;
-  background-color: #ffffff;
-  max-height: 100vh;
-  overflow-y: auto;
-
-  .custom-upload{
-    margin-left: 0;
-  }
+.modal{
+    min-height: calc( 100vh - 60px );
+    padding: 30px 0;
+    
+    .meta-modal{
+        width: 100%;
+        max-width: 500px;
+        margin: 0 auto;
+        box-shadow: 0px 7px 20px rgba(0, 0, 0, 0.1);
+        border-radius: 15px;
+        background: #ffffff;
+        .meta-modal-body{
+            font-size: 18px;
+            font-family: Proxima Nova Rg;
+            line-height: 1.4;
+            padding: 50px 30px;
+            .meta-input-group {
+                width: calc( 50% - 20px );
+                padding: 0 10px;
+                align-items: flex-start;
+                display: flex;
+                flex-wrap: wrap;
+                float: left;
+                .input-field{
+                    width: 100%;
+                }
+            }
+            .meta-upload-div {
+                width: 100%;
+                padding: 0 10px;
+            }
+        }
+        .meta-buttons{
+            width: 100%;
+            button{
+                width: 50%;
+                float: left;
+                background-color: transparent;
+                border: 0;
+                padding: 20px 10px;
+                border-top: 1px solid #eee;
+                font-size: 15px;
+                font-weight: bold;
+                font-family: Proxima Nova Rg;
+                transition: 0.2s;
+                cursor: pointer;
+            }
+            button:focus{
+                outline: 0;
+            }
+            button.agree{
+                border-right: 1px solid #eee;
+                border-bottom-left-radius: 15px;
+            }
+            button.cancel{
+                border-bottom-right-radius: 15px;
+            }
+            button.agree:hover{
+                color: #fff;
+                background-color: #4791db;
+            }
+            button.cancel:hover{
+                color: #fff;
+                background-color: #f44336;
+            }
+        }
+    }
 }
 </style>
---
+++ src/components/payment/PaymentViewDetailsModal.vue
@@ -0,0 +1,167 @@
+<template>
+<div class="modal flex-center">
+    <div class="meta-modal">
+      <div class="meta-wrap">
+        <div class="meta-modal-body flex-wrap">
+            <h3 class="mb20 modal-title">Payment Details</h3>
+            <div class="meta-body-items">
+                <div class="meta-input-group">
+                    <div class="meta-input-label">Payor's Name</div>
+                    <div class="meta-input-value">
+                        <span class="separator">:</span>
+                        John Michael Doe
+                    </div>
+                </div>
+                <div class="meta-input-group">
+                    <div class="meta-input-label">Bank Used to Pay</div>
+                    <div class="meta-input-value">
+                        <span class="separator">:</span>
+                        Security Bank
+                    </div>
+                </div>
+                <div class="meta-input-group">
+                    <div class="meta-input-label">Date of Payment</div>
+                    <div class="meta-input-value">
+                        <span class="separator">:</span>
+                        P 20,000.00
+                    </div>
+                </div>
+                <div class="meta-input-group">
+                    <div class="meta-input-label">Total Amount Paid</div>
+                    <div class="meta-input-value">
+                        <span class="separator">:</span>
+                        P 20,000.00
+                    </div>
+                </div>
+                <div class="meta-input-group">
+                    <div class="meta-input-label">Reference No.</div>
+                    <div class="meta-input-value">
+                        <span class="separator">:</span>
+                        06133649
+                    </div>
+                </div>
+                <div class="meta-input-group flex-wrap">
+                  <div class="meta-input-label">Proof of Transaction:</div>
+                  <div class="meta-input-value meta-link">
+                        <span class="separator">:</span>
+                        View Screenshot
+                  </div>
+                </div>
+              </div>
+        </div>
+        <div class="meta-buttons flex-wrap">
+            <button class="modal-button agree" @click.native="submit">VERIFY</button>
+            <button class="modal-button cancel" @click="onClickCallback(false)">CANCEL</button>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "PaymentViewDetailsModal",
+};
+</script>
+
+<style scoped lang="scss">
+.modal{
+    min-height: calc( 100vh - 60px );
+    padding: 30px 0;
+    
+    .meta-modal{
+        width: 100%;
+        max-width: 500px;
+        margin: 0 auto;
+        box-shadow: 0px 7px 20px rgba(0, 0, 0, 0.1);
+        border-radius: 15px;
+        background: #ffffff;
+        .meta-modal-body{
+            font-size: 18px;
+            font-family: Proxima Nova Rg;
+            line-height: 1.4;
+            padding: 50px 30px 40px;
+            .meta-body-items{
+                width: 100%;
+            }
+            .modal-title{
+                width: 100%;
+                text-align: left;
+                padding-left: 10px;
+                margin-bottom: 30px;
+                text-transform: uppercase;
+            }
+            .meta-input-group {
+                width: calc( 100% - 20px );
+                padding: 0 10px;
+                align-items: center;
+                display: flex;
+                margin-bottom: 20px;
+                .input-field{
+                    width: 100%;
+                }
+                .meta-input-label{
+                    width: 40%;
+                    float: left;
+                    
+                font-size: 16px;
+                font-family: Proxima Nova Rg;
+                line-height: 1.4;
+                margin-bottom: 0;
+                }
+                .meta-input-value{
+                width: 60%;
+                float: left;
+                font-size: 16px;
+                font-family: Proxima Nova Rg;
+                line-height: 1.4;
+
+                span.separator {
+                    margin-right: 20px;
+                }
+                }
+            }
+            .meta-upload-div {
+                width: 100%;
+                padding: 0 10px;
+            }
+        }
+        .meta-buttons{
+            width: 100%;
+            button{
+                width: 50%;
+                float: left;
+                background-color: transparent;
+                border: 0;
+                padding: 20px 10px;
+                border-top: 1px solid #eee;
+                font-size: 15px;
+                font-weight: bold;
+                font-family: Proxima Nova Rg;
+                transition: 0.2s;
+                cursor: pointer;
+            }
+            button:focus{
+                outline: 0;
+            }
+            button.agree{
+                border-right: 1px solid #eee;
+                border-bottom-left-radius: 15px;
+            }
+            button.cancel{
+                border-bottom-right-radius: 15px;
+            }
+            button.agree:hover{
+                color: #fff;
+                background-color: #4791db;
+            }
+            button.cancel:hover{
+                color: #fff;
+                background-color: #f44336;
+            }
+        }
+    }
+}
+
+</style>
