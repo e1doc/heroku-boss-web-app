4716e813ca35b0da1ffe13ecbd71235452cc49dc
Joshua Regio <regiojosh1@gmail.com>
2022-01-06 16:06:33 +0800

    download invoice

---
+++ src/pages/MobileDownloadInvoice.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="meta-parent-box">
+    <div class="meta-form-holder">
+      <downloadable-invoice />
+    </div>
+    <div class="meta-message-holder">
+      <h3 class="meta-message">{{ message }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import DownloadableInvoice from "@/components/payment/DownloadableInvoice";
+import DownloadableInvoice from "../components/payment/DownloadableInvoice.vue";
+export default {
+  name: "MobileDownloadInvoice",
+  components: { DownloadableInvoiceDownloadableInvoice },
+  data() {
+    return {
+      message: "Please wait, your file is now downloading...",
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const id = this.$route.params.id;
+        const token = this.$route.params.token;
+        const type = this.$route.params.type;
+
+        const result = await axios.get(
+          `${process.env.VUE_APP_API_URL}/api/download-invoice/?id=${id}`,
+          { headers: { Authorization: `jwt ${token}` } }
+        );
+
+        const soa = result.data;
+        if (soa) {
+          await this.$store.commit("setCurrentSoaObj", soa);
+          if (type === "business") {
+            await this.$store.commit(
+              "setCurrentSelectedBusiness",
+              soa.business_application
+            );
+          }
+          await this.$store.commit("setCurrentSoaType", type);
+          await this.$store.commit("setPrintInvoice", true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div.meta-parent-box {
+  width: 100%;
+  margin-top: 50px;
+  padding-bottom: 50px;
+  .meta-form-holder {
+    position: absolute;
+    opacity: 0;
+    top: -500px;
+    z-index: -1;
+  }
+  .meta-message-holder {
+    display: flex;
+    padding: 13px;
+    flex: 1;
+    .meta-message {
+      text-align: center;
+    }
+  }
+}
+</style>
